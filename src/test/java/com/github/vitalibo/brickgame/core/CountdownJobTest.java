a07bfd7e0f78c2f06c90057677711a41ad09dcc6
DeboloAurelien <auredebolo@gmail.com>
2022-01-29 19:37:55 +0100

    .

--- src/test/java/com/github/vitalibo/brickgame/core/CountdownJobTest.java
+++ src/test/java/com/github/vitalibo/brickgame/core/CountdownJobTest.java
@@ -27,7 +27,7 @@ public class CountdownJobTest {
         sleep();
         Assert.assertFalse(finished);
         Assert.assertFalse(job.isKilled());
-        Assert.assertEquals(invocations, 8);
+        Assert.assertEquals(invocations, 7);
     }
 
     @SneakyThrows
